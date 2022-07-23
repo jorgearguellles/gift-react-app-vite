8142a297a118b70fd12b36282d283c5a1c6d21db
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-07-23 15:47:29 -0500

    add/ logic to render the top 20 result regarding category to search

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,16 +1,29 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ( props ) => {
     const { category } = props;
+    const [images, setImages] = useState([]);
+
+    const getImages = async () => {
+      const newImages = await getGifs( category );
+      setImages( newImages );
+    }
 
     useEffect(()=>{
-      getGifs(category);
+      getImages();
     }, [])
     
   return (
     <>
         <h3>{category}</h3>
+        <ol>
+          {
+            images.map( ({id, title, urlImg})=>(
+              <li key={id}>{title}</li>
+            ))
+          }
+        </ol>
     </>
   )
 };
